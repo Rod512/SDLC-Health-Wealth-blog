e49db70a73add558ff43952b43aee4d1f0f478e1
Rudro Dhar <rudro149187@bsdi-bd.org>
2025-05-07 01:49:12 +0600

    port updated

--- health_blog/settings.py
+++ health_blog/settings.py
@@ -7,6 +7,9 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PORT = os.getenv('PORT', 8000)
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
