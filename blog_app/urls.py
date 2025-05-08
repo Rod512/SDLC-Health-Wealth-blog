5bd3cd037f1f52ae808e3ecbfa3cb722efe09c3e
Rudro Dhar <rudro149187@bsdi-bd.org>
2025-05-09 03:19:23 +0600

    urls configure

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("post_blog/", views.post_blog, name="post_blog"),
+    path("post_blogs/", views.post_blog, name="post_blog"),
     path("get_blogs/", views.get_blogs, name="get_blogs"),
     path("get_blogs/<int:pk>/", views.get_single_blog, name="get_blogs"),
     path('patch_blogs/<int:pk>/', views.partial_update_blog, name='update_patch'),
