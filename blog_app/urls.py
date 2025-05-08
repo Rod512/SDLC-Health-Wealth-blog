9cda364dfcb726ec9065bfef191c74c1bbd01051
Rudro Dhar <rudro149187@bsdi-bd.org>
2025-05-09 02:59:26 +0600

    urls updated

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("post_blog/", views.post_blog, name="post_blog"),
     path("get_blogs/", views.get_blogs, name="get_blogs"),
     path("get_blogs/<int:pk>/", views.get_single_blog, name="get_blogs"),
-    path('partial_update/<int:pk>/', views.partial_update_blog, name='update_patch'),
-    path('update_blog/<int:pk>/', views.update_blog, name='update_put'),
-    path('delete_blog/', views.delete_blog, name="delete_blog")
+    path('patch_blogs/<int:pk>/', views.partial_update_blog, name='update_patch'),
+    path('put_blogs/<int:pk>/', views.update_blog, name='update_put'),
+    path('delete_blogs/', views.delete_blog, name="delete_blog")
 ]
