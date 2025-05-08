a5d37cad10fe59a909b77f7d5fdde636b1c68f99
Rudro Dhar <rudro149187@bsdi-bd.org>
2025-05-08 13:17:56 +0600

    latest update

--- health_blog/settings.py
+++ health_blog/settings.py
@@ -134,3 +134,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000'
+]
+CORS_ALLOW_CREDENTIALS = True
